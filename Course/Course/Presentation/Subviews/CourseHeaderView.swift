abb8ad63a0b0cee88de1ec9633524146afdacd4d
LuisAbonce <ltorresa1701@alumno.ipn.mx>
2026-02-03 17:11:02 -0600

    feat: increase expanded height of course header view and update configuration for development environment

--- Course/Course/Presentation/Subviews/CourseHeaderView.swift
+++ Course/Course/Presentation/Subviews/CourseHeaderView.swift
@@ -22,7 +22,7 @@ struct CourseHeaderView: View {
     
     private let collapsedHorizontalHeight: CGFloat = 230
     private let collapsedVerticalHeight: CGFloat = 260
-    private let expandedHeight: CGFloat = 400
+    private let expandedHeight: CGFloat = 550
     
     private let courseRawImage: String?
     private enum GeometryName {
